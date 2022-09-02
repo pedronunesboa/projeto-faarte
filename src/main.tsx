66620fbac0eb77144d14000ad4d176d9db949b46
pedrobadb <pedyoda@gmail.com>
2022-09-01 23:28:29 -0300

    Insercao de novas páginas e import do route-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './pages/Institucional/Institucional'
-import { BrowserRouter } from 'react-router-dom';
 import './styles/navbar.css'
 import './styles/root.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 )
